0418510eabffc3cf74ccaccac758087c3672a43b
Science-Is-Real <37350546+Science-Is-Real@users.noreply.github.com>
2018-08-31 13:57:35 -0400

    V0.1.9
    GUI in progress

--- LibraryPY.py
+++ LibraryPY.py
@@ -1,18 +1,34 @@
-import GUI.py
-from TKinter import *
+from GUI import *
+from Tkinter import *
+import os
+from os.path import join
+import datetime
 bookData = open('bookData', 'w+')
 userData = open('userData.txt', 'w+')
 
+def command():
+    button('Check out', checkOut())
+    button('Check in', checkIn())
+    button('Add a book', addBook())
+    labels
+    button('Add a user', addUser())
+
 def checkOut():
     checkedOutBooks = []
-    checkedOutBooksInput = input(' Type "done" when done./nBook number: ')
-    while checkedOutBooksInput != done:
+    user = input('User number: ')
+    checkedOutBooksInput = input('Type "done" when done./nBook number: ')
+    while checkedOutBooksInput != 'done':
         checkedOutBooks.append(checkedOutBooksInput)
-        checkedOutBooksInput = input(' Type "done" when done./nBook number: ')
+        checkedOutBooksInput = input('Type "done" when done./nBook number: ')
     
     
 def checkIn():
-    
+    checkedInBooks = []
+    user = input('User number: ')
+    checkedInBooksInput = input('Type "done" when done./nBook number: ')
+    while checkedInBooksInput != 'done':
+        checkedInBooks.append(checkedInBooksInput)
+        checkedInBooksInput = input('Type "done" when done./nBook number: ')
 
 def addBook():
     bookName = input('Book name: ')
@@ -20,7 +36,7 @@ def addBook():
     bookDate = input('Publishing date: ')
     bookISBN = input('ISBN: ')
     bookNumber = input('Book number: ')
-    bookData.write(bookNumber + '=[' + bookName + ',' + bookAuthor + ',' + bookdate + ',' + bookISBN + ']') 
+    bookData.write(bookNumber + '=[' + bookName + ',' + bookAuthor + ',' + bookdate + ',' + bookISBN + 'no]') 
     
 def addUser():
     userName1 = input('Name: ')
